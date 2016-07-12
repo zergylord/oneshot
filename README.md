66949b199ae9f3ffbb9181ac0d64614ab6254aee
Steven <steven@osaro.com>
2016-07-12 13:43:12 -0700

    pretty, documented, release version

---
+++ README.md
@@ -0,0 +1,11 @@
+Replication of the basic model (No full context embeddings) is the paper "Matching Networks for One Shot Learning" (http://arxiv.org/pdf/1606.04080.pdf). 
+The currect version only works with the Omniglot dataset (included).
+
+To run, simply clone the repo, cd into the directory, and enter "python3 match_net.py"
+
+The only prerequites are:
+-Python 3
+-Tensorflow r.9
+-numpy
+
+This code has only been tested on Ubuntu 16.4 and Mac OS X, but should probably work on Windows as well.
--- match_net.py
+++ match_net.py
@@ -1,19 +1,26 @@
 import numpy as np
 import time
 cur_time = time.time()
-mb_dim = 32
-x_dim = 28
-y_dim = 20 #5 possible classes
-n_samples_per_class = 1 #5 samples of each class
-n_samples = y_dim*n_samples_per_class
-eps = 1e-10
-tie = False
-x_i_learn = True
-learning_rate = 1e-3 #1e-1
+mb_dim = 32 #training examples per minibatch
+x_dim = 28  #size of one side of square image
+y_dim = 20  #possible classes
+n_samples_per_class = 1 #samples of each class
+n_samples = y_dim*n_samples_per_class #total number of labeled samples
+eps = 1e-10 #term added for numerical stability of log computations
+tie = False #tie the weights of the query network to the labeled network
+x_i_learn = True #toggle learning for the query network
+learning_rate = 1e-1
 
 data = np.load('data.npy')
-data = np.reshape(data,[-1,20,28,28])
+data = np.reshape(data,[-1,20,28,28]) #each of the 1600 classes has 20 examples
 
+'''
+    Samples a minibatch of size mb_dim. Each training example contains
+    n_samples labeled samples, such that n_samples_per_class samples
+    come from each of y_dim randomly chosen classes. An additional example
+    one one of these classes is then chosen to be the query, and its label
+    is the target of the network.
+'''
 def get_minibatch():
     mb_x_i = np.zeros((mb_dim,n_samples,x_dim,x_dim,1))
     mb_y_i = np.zeros((mb_dim,n_samples))
@@ -56,6 +63,13 @@ y_i_ind = tf.placeholder(tf.int32,shape=[None,n_samples])
 y_i = tf.one_hot(y_i_ind,y_dim)
 y_hat_ind = tf.placeholder(tf.int32,shape=[None])
 y_hat = tf.one_hot(y_hat_ind,y_dim)
+'''
+    creates a stack of 4 layers. Each layer contains a
+    3x3 conv layers, batch normalization, retified activation,
+    and then 2x2 max pooling. The net effect is to tranform the
+    mb_dimx28x28x1 images into a mb_dimx1x1x64 embedding, the extra
+    dims are removed, resulting in mb_dimx64.
+'''
 def make_conv_net(inp,scope,reuse=False,stop_grad=False):
     with tf.variable_scope(scope) as varscope:
         if reuse: varscope.reuse_variables()
@@ -76,9 +90,15 @@ def make_conv_net(inp,scope,reuse=False,stop_grad=False):
         return tf.stop_gradient(tf.squeeze(cur_input,[1,2]))
     else:
         return tf.squeeze(cur_input,[1,2])
+'''
+    assemble a computational graph for processing minibatches of the n_samples labeled examples and one unlabeled sample.
+    All labeled examples use the same convolutional network, whereas the unlabeled sample defaults to using different parameters.
+    After using the convolutional networks to encode the input, the pairwise cos similarity is computed. The normalized version of this
+    is used to weight each label's contribution to the queried label prediction.
+'''
 scope = 'encode_x'
 x_hat_encode = make_conv_net(x_hat,scope)
-x_hat_inv_mag = tf.rsqrt(tf.clip_by_value(tf.reduce_sum(tf.square(x_hat_encode),1,keep_dims=True),eps,float("inf")))
+#x_hat_inv_mag = tf.rsqrt(tf.clip_by_value(tf.reduce_sum(tf.square(x_hat_encode),1,keep_dims=True),eps,float("inf")))
 cos_sim_list = []
 if not tie:
     scope = 'encode_x_i'
@@ -96,26 +116,27 @@ weighting = tf.nn.softmax(cos_sim)
 label_prob = tf.squeeze(tf.batch_matmul(tf.expand_dims(weighting,1),y_i))
 tf.histogram_summary('label prob',label_prob)
 
-#acc = tf.reduce_mean(tf.reduce_sum(label_prob*y_hat,1),0)
 top_k = tf.nn.in_top_k(label_prob,y_hat_ind,1)
 acc = tf.reduce_mean(tf.to_float(top_k))
 tf.scalar_summary('avg accuracy',acc)
-masked = tf.log(tf.clip_by_value(label_prob,eps,1.0))*y_hat
-correct_prob = tf.reduce_sum(masked,1)
+correct_prob = tf.reduce_sum(tf.log(tf.clip_by_value(label_prob,eps,1.0))*y_hat,1)
 loss = tf.reduce_mean(-correct_prob,0)
 tf.scalar_summary('loss',loss)
-#optim = tf.train.GradientDescentOptimizer(learning_rate)
-optim = tf.train.AdamOptimizer(learning_rate)
+optim = tf.train.GradientDescentOptimizer(learning_rate)
+#optim = tf.train.AdamOptimizer(learning_rate)
 grads = optim.compute_gradients(loss)
-#grad_summaries = [tf.histogram_summary(v.name,g) for g,v in grads]
+grad_summaries = [tf.histogram_summary(v.name,g) if g is not None else '' for g,v in grads]
 train_step = optim.apply_gradients(grads)
+
+'''
+    End of the construction of the computational graph. The remaining code runs training steps.
+'''
 
 sess = tf.Session()
 merged = tf.merge_all_summaries()
 writer = tf.train.SummaryWriter(FLAGS.summary_dir,sess.graph)
 sess.run(tf.initialize_all_variables())
 
-#mb_x_i,mb_y_i,mb_x_hat,mb_y_hat = get_minibatch()
 for i in range(int(1e7)):
     mb_x_i,mb_y_i,mb_x_hat,mb_y_hat = get_minibatch()
     feed_dict = {x_hat: mb_x_hat,
